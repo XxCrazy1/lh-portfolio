2fdf23e26e614471bfc7097bb1f863a4ccf78d69
XxCrazy <109563862+XxCrazy1@users.noreply.github.com>
2025-02-09 10:28:56 -0300

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,7 +28,7 @@ export const Hero = () => {
       <div className="relative z-10 my-20 flex justify-center">
         <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
           <h2 className="max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
-            Dynamic Web Magic com Next.js
+            Website magico dinâmico com Next.js,
           </h2>
 
           <TextGenerateEffect
